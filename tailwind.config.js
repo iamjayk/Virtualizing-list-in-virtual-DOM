6ef78ca5159bf18903e5d8905005b4c74362525e
Ajay <Ajay K>
2026-03-15 00:14:10 +1100

    Updates

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-// tailwind.config.js
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/Virtualizing-list-in-virtual-DOM/",
-  plugins: [react(), tailwindcss()],
-});
