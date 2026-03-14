376bf84c4a365ae2f44b3626a58668d0cf86ce48
Ajay <ajay8293@gmail.com>
2026-03-14 23:22:11 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Virtualizing-list-in-virtual-DOM/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
